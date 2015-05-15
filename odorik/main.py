86d133637d2496852cfb1b793c7783df6efa93a2
Michal Čihař <michal@cihar.com>
2015-05-15 11:34:32 +0200

    Remove unused import
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- odorik/main.py
+++ odorik/main.py
@@ -24,7 +24,6 @@ from __future__ import print_function
 
 import sys
 from xdg.BaseDirectory import load_config_paths
-from pprint import pformat
 from argparse import ArgumentParser
 from ConfigParser import RawConfigParser
 
