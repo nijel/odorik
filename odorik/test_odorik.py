30922612c40a0ac1c6096f8d18cd9071c9efee2b
Michal Čihař <michal@cihar.com>
2015-05-18 13:03:51 +0200

    Remove debugging code
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- odorik/test_odorik.py
+++ odorik/test_odorik.py
@@ -50,7 +50,6 @@ CALLS_BODY = (
 def sms_response(request, uri, headers):
     """httpretty SMS sending response generator"""
     params = parse_qs(request.body.decode('utf-8'))
-    print(uri, request.body, params)
     if params['sender'][0] == '5517':
         return (200, headers, 'successfully_sent 132.44')
     return (200, headers, 'error unsupported_recipient')
