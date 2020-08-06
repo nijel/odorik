e01ad6ccfda38cb004cd3ef534c5cd7c7a428030
Michal Čihař <michal@cihar.com>
2020-08-06 10:02:11 +0200

    Update list of supported and tested Python versions

--- setup.py
+++ setup.py
@@ -57,12 +57,12 @@ setup(
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
+    python_requires=">=3.6",
 
     entry_points={
         'console_scripts': ['odorik = odorik.main:main']
